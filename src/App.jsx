6fe994013234ee42060d7aa2df93559a3700b109
Mir Inayat Ahmed <161032591+Mir-Inayat@users.noreply.github.com>
2024-09-22 07:43:02 +0000

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import About from "./pages/About";
+import About from "./pages/about";
 import Navbar from "./components/Navbar";
 import HoF from "./pages/HoF";
 import Anime from "./pages/anime";
